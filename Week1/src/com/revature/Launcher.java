d2d7706e62d27cd3338843f6d3851e90d2e23634
jesusdecyrus <166780902+jesusdecyrus@users.noreply.github.com>
2024-10-01 10:17:05 -0400

    Add files via upload

---
+++ Week1/src/com/revature/Launcher.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+/**
+ * Week 1: Monday (9/30/24)
+ */
+public class Launcher {
+
+    /**
+     * Main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
