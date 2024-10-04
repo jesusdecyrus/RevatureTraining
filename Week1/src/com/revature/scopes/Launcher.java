642b684b64332ad26b6861734870371e801ca82d
Cyrus DeJesus <cbdejesu@ncsu.edu>
2024-10-04 15:10:33 -0400

    Added most of Week 1 files

--- Week1/src/com/revature/scopes/Launcher.java
+++ Week1/src/com/revature/scopes/Launcher.java
@@ -10,6 +10,42 @@ public class Launcher {
      * @param args command line arguments
      */
     public static void main(String[] args) {
+        // CLASS SCOPE
+        System.out.println();
+        System.out.println("CLASS SCOPE:");
+        System.out.println(ScopeTester.sharedBetweenObjects1);
+        System.out.println(ScopeTester.sharedBetweenObjects2);
+        staticMethod();
 
+        // INSTANCE SCOPE
+        System.out.println();
+        System.out.println("INSTANCE SCOPE:");
+        ScopeTester st = new ScopeTester();
+        System.out.println(st.specificToThisObject);
+
+        // BLOCK SCOPE
+        System.out.println();
+        System.out.println("BLOCK SCOPE:");
+        boolean canRun = true;
+        if (canRun) {
+            System.out.println("I can run in the block scope.");
+        }
+        else {
+            System.out.println("I cannot run in the block scope.");
+        }
+
+        // METHOD SCOPE
+        System.out.println();
+        System.out.println("METHOD SCOPE:");
+        methodScope();
+    }
+
+    public static void staticMethod() {
+        System.out.println("Static makes me run in the main method.");
+    }
+
+    public static void methodScope() {
+        int garbageInt = 20;
+        System.out.println(garbageInt);
     }
 }
--- Week1/src/com/revature/scopes/ScopeTester.java
+++ Week1/src/com/revature/scopes/ScopeTester.java
@@ -6,7 +6,7 @@ package com.revature.scopes;
 public class ScopeTester {
 
     /** CLASS SCOPE (STATIC) - static variables are shared between objects */
-    private static int sharedBetweenObjects = 10;
+    public static int sharedBetweenObjects1 = 10;
     public static int sharedBetweenObjects2;
 
     /** INSTANCE SCOPE (OBJECT) - variables that can only be shared to this object */
