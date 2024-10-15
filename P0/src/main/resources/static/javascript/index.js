c856203b613ba947d84c178b47b9cd9f5d6f9bd4
Cyrus DeJesus <cbdejesu@ncsu.edu>
2024-10-15 17:12:08 -0400

    Added template for catching a pokemon in the frontend

--- P0/src/main/resources/static/javascript/index.js
+++ P0/src/main/resources/static/javascript/index.js
@@ -1,9 +1,122 @@
+// Global Current Random Encounter
+var currentEncounterPokemon;
+
 /**
  * Clear all child elements of allPokemon container
  */
 function clearAllPokemon() {
   const allPokemon = document.getElementById('allPokemon');
   allPokemon.innerHTML = '';
+}
+
+/**
+ * Random encounter button
+ */
+function randomEncounter() {
+  // Clear allPokemon container
+  clearAllPokemon();
+  
+  // Communicate with Java
+  fetch('/randomPokemon', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Set currentEncounterPokemon
+    currentEncounterPokemon = data;
+    
+    // Create and append the div for the random pokemon
+    const pokemonDiv = document.createElement('div');
+    pokemonDiv.className = 'onePokemon';
+    pokemonDiv.innerHTML = `<a>${data.name}</a>
+                            <p><strong>Level:</strong> ${data.level}</p>
+                            <p><strong>Type:</strong> ${data.type}</p>
+                            <p><strong>Pokedex Number:</strong> ${data.pokemonID}</p>
+                            <p><strong>Gender:</strong> ${data.gender}</p>
+                            <p><strong>Shiny:</strong> ${data.shiny ? 'Yes' : 'No'}</p>`;
+    document.getElementById('allPokemon').appendChild(pokemonDiv);
+
+    // Append button div
+    const buttonDiv = document.createElement('div');
+    buttonDiv.className = 'reButtons';
+    buttonDiv.innerHTML = `<button class="catchButton" onclick="catchPokemon()">Catch</button>
+                           <button class="runButton" onclick="randomEncounter()">Run</button>`;
+     document.getElementById('allPokemon').appendChild(buttonDiv);
+  })
+  .catch(error => {
+    console.error('Unable to Random Encounter: ', error);
+  });
+}
+
+/**
+ * Catch pokemon button
+ */
+function catchPokemon() {
+  // Clear allPokemon container
+  clearAllPokemon();
+
+  // HARD CODED CHANGE LATER
+  currentEncounterPokemon.trainerID = 1;
+
+  // Communicate with Java
+  fetch('/addPokemon', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(currentEncounterPokemon) 
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error with network: ' + response.statusText);
+    }
+      return response.json();
+  })
+  .then(data => {
+      console.log('Successfully Added Pokemon!');
+  })
+  .catch(error => {
+      console.error('Error Adding Pokemon: ', error);
+  });
+}
+
+/**
+ * View owned pokemon button
+ */
+function viewOwnedPokemon() {
+  // Clear allPokemon container
+  clearAllPokemon();
+
+  // HARD CODED CHANGE LATER
+  var trainerID = 1;
+  
+  // Communicate with Java
+  fetch('/pokemonTrainer/' + trainerID, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Iterate through each array
+    data.forEach(pokemon => {
+      const pokemonDiv = document.createElement('div');
+      pokemonDiv.className = 'onePokemon';
+
+      // Create and set innerHTML for one pokemon
+      pokemonDiv.innerHTML = `<a>${pokemon.name}</a>
+        <p><strong>Level:</strong> ${pokemon.level}</p>
+        <p><strong>Type:</strong> ${pokemon.type}</p>
+        <p><strong>Pokemon ID:</strong> ${pokemon.pokemonID}</p>
+        <p><strong>Trainer ID:</strong> ${pokemon.trainerID}</p>
+        <p><strong>Gender:</strong> ${pokemon.gender}</p>
+        <p><strong>Shiny:</strong> ${pokemon.shiny ? 'Yes' : 'No'}</p>`;
+
+      // Append the new div to the allPokemon container
+      document.getElementById('allPokemon').appendChild(pokemonDiv);
+    })
+  })
+  .catch(error => {
+    console.error('Unable To View All Pokemon: ', error);
+  });
 }
 
 /**
@@ -38,46 +151,6 @@ function viewAllPokemon() {
     })
   })
   .catch(error => {
-    console.error('Unable to view all Pokemon: ', error);
+    console.error('Unable To View All Pokemon: ', error);
   });
 }
-
-/**
- * Random encounter button
- */
-function randomEncounter() {
-  // Clear allPokemon container
-  clearAllPokemon();
-  
-  // Communicate with Java
-  fetch('/randomPokemon', {
-    method: 'GET'
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Create and append the div for the random pokemon
-    const pokemonDiv = document.createElement('div');
-    pokemonDiv.className = 'onePokemon';
-    pokemonDiv.innerHTML = `<a>${data.name}</a>
-                            <p><strong>Level:</strong> ${data.level}</p>
-                            <p><strong>Type:</strong> ${data.type}</p>
-                            <p><strong>Pokedex Number:</strong> ${data.pokemonID}</p>
-                            <p><strong>Gender:</strong> ${data.gender}</p>
-                            <p><strong>Shiny:</strong> ${data.shiny ? 'Yes' : 'No'}</p>`;
-    document.getElementById('allPokemon').appendChild(pokemonDiv);
-
-    // Append button div
-    const buttonDiv = document.createElement('div');
-    buttonDiv.className = 'reButtons';
-    buttonDiv.innerHTML = `<button class="catchButton">Catch</button>
-                           <button class="runButton" onclick="randomEncounter()">Run</button>`;
-     document.getElementById('allPokemon').appendChild(buttonDiv);
-  })
-  .catch(error => {
-    console.error('Unable to random encounter: ', error);
-  });
-}
-
-setTimeout(() => {
-  pokemonDiv.classList.add('show');
-}, 10);
